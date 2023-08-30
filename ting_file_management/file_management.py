117598b7f6cc2097fc22e08acf0002d37811a229
SamuelRocha91 <samuel_sr@hotmail.com.br>
2023-08-30 19:54:37 -0300

    requisito 2 - arquivo txt leitura

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,13 @@
+import sys
+
+
 def txt_importer(path_file):
     """Aqui irá sua implementação"""
+    try:
+        with open(path_file, "r") as file:
+            if '.txt' not in path_file:
+                sys.stderr.write("Formato inválido")
+            document = file.read().split('\n')
+            return document
+    except FileNotFoundError:
+        sys.stderr.write(f"Arquivo {path_file} não encontrado\n")

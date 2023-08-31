870d90708332299b7b8ff9f7ca72534ed73814e1
SamuelRocha91 <samuel_sr@hotmail.com.br>
2023-08-30 21:34:46 -0300

    requisito 03

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,19 @@
+import sys
+
+
 def process(path_file, instance):
     """Aqui irá sua implementação"""
+    if f"{path_file}" in instance.data:
+        return
+    instance.enqueue(f"{path_file}")
+    with open(path_file, "r") as file:
+        document = file.read().split('\n')
+        dict = {
+          "nome_do_arquivo": f"{path_file}",
+          "qtd_linhas": len(document),
+          "linhas_do_arquivo": document,
+         }
+    sys.stdout.write(f"{dict}")
 
 
 def remove(instance):

6973b94cf23a4a71463d696495ba4c6f488c3510
SamuelRocha91 <samuel_sr@hotmail.com.br>
2023-08-31 19:11:25 -0300

    requisito 07

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,6 +1,34 @@
+
+
 def exists_word(word, instance):
     """Aqui irá sua implementação"""
+    word_search = []
+    for i in range(len(instance.data)):
+        with open(instance.data[i], "r") as file:
+            lines = file.read().split('\n')
+            dict = {
+              "nome_do_arquivo": f"{instance.data[i]}",
+              "qtd_linhas": len(lines),
+              "linhas_do_arquivo": lines,
+             }
+            new_dict = search_in_lines(dict, word)
+            word_search.append(new_dict)
+    if len(word_search) == 1 and not word_search[0]["ocorrencias"]:
+        return []
+    return word_search
 
 
 def search_by_word(word, instance):
     """Aqui irá sua implementação"""
+
+
+def search_in_lines(file, word):
+    occurrences = []
+    for i in range(len(file["linhas_do_arquivo"])):
+        if word in file["linhas_do_arquivo"][i].lower():
+            occurrences.append({"linha": i + 1})
+    return {
+        "palavra": word,
+        "arquivo": file["nome_do_arquivo"],
+        "ocorrencias": occurrences,
+    }
